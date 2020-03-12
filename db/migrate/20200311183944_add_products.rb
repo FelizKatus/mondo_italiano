55e0e23f15a9bb2fb409dafea5017e6cae8db07d
FelizKatus <felizkatus@gmail.com>
2020-03-12 10:57:23 +0100

    Few add_products migration improvements

--- db/migrate/20200311183944_add_products.rb
+++ db/migrate/20200311183944_add_products.rb
@@ -1,9 +1,15 @@
 class AddProducts < ActiveRecord::Migration[6.0]
   def change
     Product.create :title => 'Hawaiian', :description => 'This is hawaiian pizza.', :size => 30, :is_spicy => false, :is_veg => false, :is_offer => false, :path_to_image => '/images/hawaiian.jpg', :price => 5
+    Product.create :title => 'Hawaiian', :description => 'This is hawaiian pizza.', :size => 45, :is_spicy => false, :is_veg => false, :is_offer => false, :path_to_image => '/images/hawaiian.jpg', :price => 8
+    Product.create :title => 'Hawaiian', :description => 'This is hawaiian pizza.', :size => 50, :is_spicy => false, :is_veg => false, :is_offer => false, :path_to_image => '/images/hawaiian.jpg', :price => 10
 
-    Product.create :title => 'Pepperoni', :description => 'Nice pepperoni pizza.', :size => 30, :is_spicy => false, :is_veg => false, :is_offer => false, :path_to_image => '/images/pepperoni.jpg', :price => 15
+    Product.create :title => 'Pepperoni', :description => 'Nice pepperoni pizza.', :size => 30, :is_spicy => true, :is_veg => false, :is_offer => true, :path_to_image => '/images/pepperoni.jpg', :price => 15
+    Product.create :title => 'Pepperoni', :description => 'Nice pepperoni pizza.', :size => 45, :is_spicy => true, :is_veg => false, :is_offer => true, :path_to_image => '/images/pepperoni.jpg', :price => 18
+    Product.create :title => 'Pepperoni', :description => 'Nice pepperoni pizza.', :size => 50, :is_spicy => true, :is_veg => false, :is_offer => true, :path_to_image => '/images/pepperoni.jpg', :price => 20
 
-    Product.create :title => 'Veg', :description => 'Amazing vegetarian pizza.', :size => 30, :is_spicy => false, :is_veg => false, :is_offer => false, :path_to_image => '/images/veg.jpg', :price => 10
+    Product.create :title => 'Veg', :description => 'Amazing vegetarian pizza.', :size => 30, :is_spicy => false, :is_veg => true, :is_offer => false, :path_to_image => '/images/veg.jpg', :price => 10
+    Product.create :title => 'Veg', :description => 'Amazing vegetarian pizza.', :size => 45, :is_spicy => false, :is_veg => true, :is_offer => false, :path_to_image => '/images/veg.jpg', :price => 12
+    Product.create :title => 'Veg', :description => 'Amazing vegetarian pizza.', :size => 50, :is_spicy => false, :is_veg => true, :is_offer => false, :path_to_image => '/images/veg.jpg', :price => 15
   end
 end
