067fea333b42593042bbbfda0b24f31bd84a02ff
FelizKatus <felizkatus@gmail.com>
2020-03-17 19:17:23 +0100

    Add function get_items_quantity()

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-// Mondo Italiano App :: Custom JS
+// Mondo Italiano App :: JS
 
 function add_to_cart(id) {
   let key = 'product_' + id
@@ -6,4 +6,19 @@ function add_to_cart(id) {
   let p = localStorage.getItem(key)
   p = p * 1 + 1 // One click add one product to cart
   localStorage.setItem(key, p)
+}
+
+function get_items_quantity() {
+  let quantity = 0
+
+  for(let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i)
+    let value = localStorage.getItem(key)
+
+    if(key.indexOf('product_') == 0) {
+      quantity = quantity + value * 1
+    }
+  }
+
+  return quantity
 }
